27043567074fe00211097e27a9ed52146808ec1a
MaximKM-62 <37383153+MaximKM-62@users.noreply.github.com>
2019-12-25 18:29:58 +0200

    Create dto.py

---
+++ Kursova/LecturebotDAL/models/dto.py
@@ -0,0 +1,8 @@
+class CourseworkDTO:
+
+    def __init__(self, initalization_num,cwname,research_direction,mark,gradebook_number):
+        self.initalization_num = initalization_num
+        self.cwname = cwname
+        self.research_direction = research_direction
+        self.mark = mark
+        self.gradebook_number = gradebook_number
