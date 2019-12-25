25c5fb0a95c2b168cdad02a6957e7746c608b606
MaximKM-62 <37383153+MaximKM-62@users.noreply.github.com>
2019-12-25 18:37:20 +0200

    Add files via upload

---
+++ Kursova/LecturebotDAL/dbconnection.py
@@ -0,0 +1,6 @@
+BASE = 'postgresql+psycopg2'
+USERNAME = 'aeeneklhkrnneh'
+PASSWORD = '178049f753ac3154137f98c8d0bd4045ab19819cf294d73ce59e9b510c9e248f'
+HOST = 'ec2-107-21-255-181.compute-1.amazonaws.com'
+PORT = '5432'
+DATABASE = 'd1joakede3ro0p'
