fdc95e0d9e50ebfe30cc2fdbe4e035e61d45ce91
Miracle1111 <annchaly8@gmail.com>
2019-12-26 13:46:11 +0200

    fixiks

--- forms/scedule_form.py
+++ forms/scedule_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField, IntegerField, SelectField
+from wtforms import StringField, SubmitField, IntegerField, SelectField, HiddenField
 from wtforms import validators
 from dao.db import PostgresDb
 from dao.orm.entities import *
@@ -8,6 +8,7 @@ from datetime import date
 db = PostgresDb()
 
 class SceduleForm(Form):
+    old_group = HiddenField()
     STATE_CHOICES = [('8:30-10:05', '8:30-10:05'), ('10:20-12:00', '10:20-12:00'), ('12:15-13:55', '12:15-13:55'), ('14:15-15:55', '14:15-15:55')]
     STATE_CH =[('Monday', 'Monday'), ('Tuesday', 'Tuesday'), ('Wednesday', 'Wednesday'), ('Thursday', 'Thursday'), ('Friday', 'Friday')]
     group_id_fk = IntegerField("group id: ", [
