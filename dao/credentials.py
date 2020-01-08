5b3ad6f60642e6f9b42700cd50777297f056cd4b
Zlata Khodos <z.khodos-2022@kpi.ua>
2020-01-08 09:25:59 +0200

    Update credentials.py

--- dao/credentials.py
+++ dao/credentials.py
@@ -1,9 +1,9 @@
 import os
 
-username = 'xujoczxbviwzjo'
-password = '83a4ab01c63234f37abb82fabf8294c7f26494fdd92b567ead595ea821a3abba'
-host = 'ec2-174-129-24-148.compute-1.amazonaws.com'
+username = 'ullxgjzmgkbmwt'
+password = '1e9fadb6b31275cea7047c47ca3ac93a66048caa157dde98c04d0db271bade9d'
+host = 'ec2-54-195-252-243.eu-west-1.compute.amazonaws.com'
 port = '5432'
-database = 'dbriles54vm09o'
+database = 'd24i9iidjpn32l'
 DATABASE_URI = os.getenv("DATABASE_URL",
-                         'postgres://xujoczxbviwzjo:83a4ab01c63234f37abb82fabf8294c7f26494fdd92b567ead595ea821a3abba@ec2-174-129-24-148.compute-1.amazonaws.com:5432/dbriles54vm09o')
+                         'postgres://ullxgjzmgkbmwt:1e9fadb6b31275cea7047c47ca3ac93a66048caa157dde98c04d0db271bade9d@ec2-54-195-252-243.eu-west-1.compute.amazonaws.com:5432/d24i9iidjpn32l')
